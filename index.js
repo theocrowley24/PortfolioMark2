cdf976be54b1c4fb206a6cdbdf5572b182de9d68
Theo <theocrowley24@gmail.com>
2020-02-15 12:17:58 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+/*
+* Hamburger menu
+*/
+
+let hamburgerButton = document.getElementById('hamburger-button');
+let hamburgerMenu = document.getElementById('hamburger');
+
+let showingHamburger = false;
+
+hamburgerButton.addEventListener('click', function (event) {
+    if (showingHamburger) {
+        hamburgerMenu.classList.remove('hamburger-displayed');
+        hamburgerMenu.classList.add('hamburger-not-displayed');
+        showingHamburger = false;
+    } else {
+        hamburgerMenu.classList.remove('hamburger-not-displayed');
+        hamburgerMenu.classList.add('hamburger-displayed');
+        showingHamburger= true;
+    }
+});
